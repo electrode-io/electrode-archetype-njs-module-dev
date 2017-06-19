6c07a66376720fdc87136bf5ec919446e4becabc
Joel Chen <joel123@gmail.com>
2017-06-19 16:09:45 -0700

    clappify and nyc

--- README.md
+++ README.md
@@ -10,19 +10,19 @@ Within your project, run:
 $ npm install --save-dev electrode-archetype-njs-module-dev
 ```
 
-Add a `gulpfile.js` with the following code:
+Add a `clap.js` with the following code:
 
 ```js
 require("electrode-archetype-njs-module-dev")();
 ```
 
-You need to install `gulp` globally to invoke the build tasks:
+You need to install `xclap-cli` globally for the `clap` command to invoke the build tasks:
 
 ```sh
-$ npm install -g gulp
+$ npm install -g xclap-cli
 ```
 
-Run `gulp` to see a list of tasks.
+Run `clap` to see a list of tasks.
 
 ## Project Structure
 
@@ -41,3 +41,5 @@ test/
 ```
 
 Built with :heart: by [Team Electrode](https://github.com/orgs/electrode-io/people) @WalmartLabs.
+
+[xclap-cli]: https://www.npmjs.com/package/xclap-cli
--- arch-clap.js
+++ arch-clap.js
@@ -1,11 +1,20 @@
 "use strict";
 const Path = require("path");
-const gulpHelper = require("electrode-gulp-helper");
+const xsh = require("xsh");
 const fs = require("fs");
 
+if (process.argv[1].indexOf("gulp") >= 0) { 
+  const cmd = `clap ${process.argv.slice(2).join(" ")}`; 
+  console.log(`\nPlease use clap to run archetype commands.`); 
+  console.log(`\nie:  ${cmd}`); 
+  const icmd = `'npm i -g xclap-cli'`; 
+  console.log(`\nIf you haven't done so, please run ${icmd}\n`); 
+  process.exit(1); 
+} 
+
 function setupPath() {
-  gulpHelper.envPath.addToFront(Path.resolve("node_modules/.bin"));
-  gulpHelper.envPath.addToFront(Path.join(__dirname, "node_modules/.bin"));
+  xsh.envPath.addToFront(Path.resolve("node_modules/.bin"));
+  xsh.envPath.addToFront(Path.join(__dirname, "node_modules/.bin"));
 }
 
 let eslintDir = `${__dirname}/config/eslint`;
@@ -24,14 +33,15 @@ const tasks = {
   "lint": [["lint-lib", "lint-test"]],
   "test": [["lint-lib", "lint-test", "test-only"]],
   "test-only": `mocha -c --opts ${__dirname}/config/test/mocha.opts test/spec`,
-  "test-cov": `istanbul cover -x gulpfile.js -x "dist/**" --include-all-sources _mocha -- -c --opts ${__dirname}/config/test/mocha.opts test/spec`,
+  "test-cov": `nyc --all --reporter=lcov --reporter=text --reporter=text-summary -x coverage -x \"*clap.js\" -x gulpfile.js -x dist -x test clap test-only`,
   "test-ci": ["test-cov"],
   "check": ["lint", "test-cov"],
   "check-ci": ["lint", "test-ci"]
 };
 
-module.exports = function (gulp) {
+module.exports = function (xclap) {
   setupPath();
   process.env.FORCE_COLOR = "true";
-  gulpHelper.loadTasks(tasks, gulp || require("gulp"));
+  xclap = xclap || require("xclap");
+  xclap.load("electrode", tasks);
 };
